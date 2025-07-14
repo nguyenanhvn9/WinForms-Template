76a0f13c5c541b30a2f95a107d84dd1fcd882f54
BuiDucHieu-2180603383 <buiduchieu522003@gmail.com>
2025-07-15 00:08:20 +0700

    2180603383__BuiDucHieu_Tuan3

---
+++ Tuan03/2180603383_BuiDucHieu_Tuan3/Program.cs
@@ -0,0 +1,21 @@
+﻿using _2180603383_BuiDucHieu_Tuan3.Tuan3_Bai2;
+using System;
+using System.Windows.Forms;
+using Tuan3_Bai2;
+
+
+namespace _2180603383_BuiDucHieu_Tuan3
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form()); // Gọi Form1, không phải Form (trống)
+        }
+    }
+
+   
+}
---
+++ Tuan03/2180603383_BuiDucHieu_Tuan3/Tuan3_Bai2/frmRegister.cs
@@ -0,0 +1,20 @@
+namespace Tuan3_Bai2
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
